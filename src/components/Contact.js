88280014e814ba985e2b1c0f9a2cc9abd5d5b188
Jeihan Praja Yudanto <jeihan.yudanto@gmail.com>
2022-06-19 14:00:08 +0700

    challenge-09-ver-1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
+// import contacts from "./data/contacts.json";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <div className="Contact">
+      <img className="profPic" src={props.photo} alt="profile"></img>
+      <section className="detailContact">
+        <h3>{props.name}</h3>
+        <p>{props.phone}</p>
+        <p>{props.email}</p>
+      </section>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div className="Header">
+      <h2>Call a Friend</h2>
+      <p>your friendly contact app</p>
+      <p>
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+      </p>
+    </div>
+  );
+};
 
 export default Header;
